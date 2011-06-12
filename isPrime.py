d1975ee388aa22ec4068417a1f7bac5080d4b5c7
ZhangJicai <plakitboy@gmail.com>
2011-06-12 22:30:35 +0800

    if a number is Prime

---
+++ isPrime.py
@@ -0,0 +1,13 @@
+def isPrime(value):
+	if value==1:
+		return False
+	else:
+		for i in xrange(2, value):
+			if value%i==0:
+				return False
+			else:
+				return True
+
+if __name__=='__main__':
+	if isPrime(929):
+		print 'hello'
